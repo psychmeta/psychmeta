37e1327dd7de9f0d6b008d3baf75e7380a511516
Brenton M. Wiernik <bwiernik@users.noreply.github.com>
2019-06-04 15:33:35 -0400

    Remove base filter function
     It's unnecessary alongside the NextMethod() in filter.ma_psychmeta()

--- R/methods_dplyr_ma_psychmeta.R
+++ R/methods_dplyr_ma_psychmeta.R
@@ -1,18 +1,3 @@
-#### filter ####
-#' Return rows with matching conditions
-#' 
-#' Use filter() find rows/cases where conditions are true. Unlike base subsetting with [, rows where the condition evaluates to NA are dropped.
-#' See the dplyr::filter help for more information about the filter() function. 
-#' 
-#' @param .data A tbl. All main verbs are S3 generics and provide methods for tbl_df(), dtplyr::tbl_dt() and dbplyr::tbl_dbi().
-#' @param ... Logical predicates defined in terms of the variables in .data. Multiple conditions are combined with &. Only rows where the condition evaluates to TRUE are kept.
-#'
-#' @export 
-#' @keywords internal
-filter <- function(.data, ...){
-     dplyr::filter(.data, ...)
-}
-
 #' @export
 #' @keywords internal
 #' @importFrom dplyr filter
@@ -50,12 +35,12 @@ filter_if.ma_psychmeta <- function(.tbl, predicate, .vars_predicate){
 #' @method select ma_psychmeta
 select.ma_psychmeta <- function(.data, ...){
      .data <- reattribute(.data, NextMethod())
-     
+
      needed_cols <- c("analysis_id", "analysis_type", "meta_tables", "escalc")
      correct_cols <- needed_cols %in% colnames(.data)
      if(!all(correct_cols))
           warning("You have removed the following critical columns: ", paste(needed_cols[!correct_cols], collapse = ", "), call. = FALSE)
-     
+
      .data
 }
 
@@ -64,12 +49,12 @@ select.ma_psychmeta <- function(.data, ...){
 #' @method select_all ma_psychmeta
 select_all.ma_psychmeta <- function(.tbl, .funs = list(), ...){
      .data <- reattribute(.data, NextMethod())
-     
+
      needed_cols <- c("analysis_id", "analysis_type", "meta_tables", "escalc")
      correct_cols <- needed_cols %in% colnames(.data)
      if(!all(correct_cols))
           warning("You have removed the following critical columns: ", paste(needed_cols[!correct_cols], collapse = ", "), call. = FALSE)
-     
+
      .data
 }
 
@@ -78,12 +63,12 @@ select_all.ma_psychmeta <- function(.tbl, .funs = list(), ...){
 #' @method select_at ma_psychmeta
 select_at.ma_psychmeta <- function(.tbl, .vars, .funs = list(), ...){
      .tbl <- reattribute(.tbl, NextMethod())
-     
+
      needed_cols <- c("analysis_id", "analysis_type", "meta_tables", "escalc")
      correct_cols <- needed_cols %in% colnames(.tbl)
      if(!all(correct_cols))
           warning("You have removed the following critical columns: ", paste(needed_cols[!correct_cols], collapse = ", "), call. = FALSE)
-     
+
      .tbl
 }
 
@@ -92,12 +77,12 @@ select_at.ma_psychmeta <- function(.tbl, .vars, .funs = list(), ...){
 #' @method select_if ma_psychmeta
 select_if.ma_psychmeta <- function(.tbl, .predicate, .funs = list(), ...){
      .tbl <- reattribute(.tbl, NextMethod())
-     
+
      needed_cols <- c("analysis_id", "analysis_type", "meta_tables", "escalc")
      correct_cols <- needed_cols %in% colnames(.tbl)
      if(!all(correct_cols))
           warning("You have removed the following critical columns: ", paste(needed_cols[!correct_cols], collapse = ", "), call. = FALSE)
-     
+
      .tbl
 }
 
@@ -109,12 +94,12 @@ select_if.ma_psychmeta <- function(.tbl, .predicate, .funs = list(), ...){
 #' @method rename ma_psychmeta
 rename.ma_psychmeta <- function(.data, ...){
      .data <- reattribute(.data, NextMethod())
-     
+
      needed_cols <- c("analysis_id", "analysis_type", "meta_tables", "escalc")
      correct_cols <- needed_cols %in% colnames(.data)
      if(!all(correct_cols))
           warning("You have removed the following critical columns: ", paste(needed_cols[!correct_cols], collapse = ", "), call. = FALSE)
-     
+
      .data
 }
 
@@ -123,12 +108,12 @@ rename.ma_psychmeta <- function(.data, ...){
 #' @method rename_all ma_psychmeta
 rename_all.ma_psychmeta <- function(.tbl, .funs = list(), ...){
      .data <- reattribute(.data, NextMethod())
-     
+
      needed_cols <- c("analysis_id", "analysis_type", "meta_tables", "escalc")
      correct_cols <- needed_cols %in% colnames(.data)
      if(!all(correct_cols))
           warning("You have removed the following critical columns: ", paste(needed_cols[!correct_cols], collapse = ", "), call. = FALSE)
-     
+
      .data
 }
 
@@ -137,12 +122,12 @@ rename_all.ma_psychmeta <- function(.tbl, .funs = list(), ...){
 #' @method rename_at ma_psychmeta
 rename_at.ma_psychmeta <- function(.tbl, .vars, .funs = list(), ...){
      .tbl <- reattribute(.tbl, NextMethod())
-     
+
      needed_cols <- c("analysis_id", "analysis_type", "meta_tables", "escalc")
      correct_cols <- needed_cols %in% colnames(.tbl)
      if(!all(correct_cols))
           warning("You have removed the following critical columns: ", paste(needed_cols[!correct_cols], collapse = ", "), call. = FALSE)
-     
+
      .tbl
 }
 
@@ -151,12 +136,12 @@ rename_at.ma_psychmeta <- function(.tbl, .vars, .funs = list(), ...){
 #' @method rename_if ma_psychmeta
 rename_if.ma_psychmeta <- function(.tbl, .predicate, .funs = list(), ...){
      .tbl <- reattribute(.tbl, NextMethod())
-     
+
      needed_cols <- c("analysis_id", "analysis_type", "meta_tables", "escalc")
      correct_cols <- needed_cols %in% colnames(.tbl)
      if(!all(correct_cols))
           warning("You have removed the following critical columns: ", paste(needed_cols[!correct_cols], collapse = ", "), call. = FALSE)
-     
+
      .tbl
 }
 
@@ -304,7 +289,7 @@ transmute_if.ma_psychmeta <- function(.tbl, .predicate, .funs, ...){
 #' @method subset ma_psychmeta
 subset.ma_psychmeta <- function (x, subset, select, drop = FALSE, ...){
      x <- reattribute(x, NextMethod())
-     
+
      needed_cols <- c("analysis_id", "analysis_type", "meta_tables", "escalc")
      correct_cols <- needed_cols %in% colnames(x)
      if(!all(correct_cols))
@@ -317,11 +302,11 @@ subset.ma_psychmeta <- function (x, subset, select, drop = FALSE, ...){
 #' @method [ ma_psychmeta
 `[.ma_psychmeta` <- function(x, i = rep(TRUE, nrow(x)), j = rep(TRUE, ncol(x)), drop = if (missing(i)) TRUE else ncol(x) == 1){
      x <- reattribute(x, NextMethod())
-     
+
      needed_cols <- c("analysis_id", "analysis_type", "meta_tables", "escalc")
      correct_cols <- needed_cols %in% colnames(x)
      if(!all(correct_cols))
           warning("You have removed the following critical columns: ", paste(needed_cols[!correct_cols], collapse = ", "), call. = FALSE)
-     
+
      x
-} 
+}
