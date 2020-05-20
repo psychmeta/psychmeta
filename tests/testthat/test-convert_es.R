99c4fbd9f731d093d6c868eb64be0e140d47884d
wesley4546 <wesley.gardiner4546@gmail.com>
2020-05-19 21:32:37 -0400

    Update test-convert_es.R

--- tests/testthat/test-convert_es.R
+++ tests/testthat/test-convert_es.R
@@ -3,10 +3,18 @@ context("adjust_n")
 library(psychmeta)
 
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("convert_es", {
+
+  # Creating variables
+  test_d_r_actual <- convert_es(es = 1, input_es = "d", output_es = "r", n1 = 50, n2 = 50) #from vignette
+  test_d_r_made_metainput <- data.frame(
+    r = c(0.4472136),
+    n_effective = c(100),
+    var_e = c(0.006431956)
+  )
+
+  # Testing
+  expect_equal(test_d_r_actual$meta_input, test_d_r_made_metainput, tolerance = 1e-7)
 })
 
-
-t <- convert_es(es = 1,input_es = "d", output_es = "d", n1 = 50)$meta_input
 
