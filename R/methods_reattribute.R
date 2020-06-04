bb5b3f70e3aa12a8a2e362a83b9bc70cce573c1c
Brenton M. Wiernik <bwiernik@users.noreply.github.com>
2020-06-04 08:53:13 -0400

    Remove one more S4 class

--- R/methods_reattribute.R
+++ R/methods_reattribute.R
@@ -5,7 +5,6 @@
 #'
 #' @return \code{result}, now with class/attributes restored.
 #' @export
-#' @exportMethod reattribute
 reattribute <- function(x, result) {
      UseMethod('reattribute')
 }
@@ -32,10 +31,10 @@ reattribute.ma_psychmeta <- function(x, result) {
      result_att <- attributes(result)
      x_att$class <- unique(c(class(x)[[1]], class(result)))
      x_att$groups <- result_att$groups
-     
+
      .preserve_new_atts <- c("names", "row.names", "vars", "drop", "indices", "group_sizes", "biggest_group_size", "labels")
      for(i in .preserve_new_atts) x_att[[i]] <- result_att[[i]]
-     
+
      attributes(result) <- x_att
      result
 }
@@ -49,10 +48,10 @@ reattribute.ma_table <- function(x, result) {
      result_att <- attributes(result)
      x_att$class <- unique(c(class(x)[[1]], class(result)))
      x_att$groups <- result_att$groups
-     
+
      .preserve_new_atts <- c("names", "row.names", "vars", "drop", "indices", "group_sizes", "biggest_group_size", "labels")
      for(i in .preserve_new_atts) x_att[[i]] <- result_att[[i]]
-     
+
      attributes(result) <- x_att
      result
 }
