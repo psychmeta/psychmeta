dd763155332d5fd31024c44b92f1bfcc9c6bfae3
Wesley G <56905659+wesley4546@users.noreply.github.com>
2020-06-29 13:21:26 -0400

    gave scalar_arg_warning better annotation

--- tests/testthat/test-scalar_arg_warning.R
+++ tests/testthat/test-scalar_arg_warning.R
@@ -1,8 +1,8 @@
-#' Testing the adjust_n_r and adjust_n_d functions
+# Testing the adjust_n_r and adjust_n_d functions
 
 library(psychmeta)
 
-test_that("checking for warning message", {
+test_that("checking for iff length(arg) is > 1", {
   # Testing
   expect_warning(scalar_arg_warning(arg = c(1, 2), arg_name = "test"))
   expect_equal(scalar_arg_warning(arg = 1, arg_name = "test"), 1)
