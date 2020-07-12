2667ff348db0984062079bd8c94671755aa23df3
Wesley G <56905659+wesley4546@users.noreply.github.com>
2020-07-12 15:58:06 -0400

    development of saving .rda files

--- tests/testthat/test-collapse_data_list.R
+++ tests/testthat/test-collapse_data_list.R
@@ -1,5 +1,22 @@
-# # Testing the .collpase_data_list function in ma_r
-# ##### IN PROGRESS ######
+# Testing the .collpase_data_list function in ma_r
+
+library(psychmeta)
+
+test_that("testing no moderators specified", {
+  
+  collapse_data_list_spec
+  
+  
+  
+  
+  
+})
+
+
+# Nonsence testing --------------------------------------------------------
+
+# Testing the .collpase_data_list function in ma_r
+##### IN PROGRESS ######
 # library(psychmeta)
 # 
 # test_data <-
@@ -22,6 +39,45 @@
 #   rxx = rxxi,
 #   ryy = ryyi,
 #   sample_id = "Study",
-#   data = test_data
+#   data = test_data,
+#   moderators = c("Complexity"), cat_moderators = FALSE
 # )
 # 
+# genuine_collapsed_data_list <-
+#   by(1:length(duplicates$analysis_id),
+#      duplicates$analysis_id,
+#      .collapse_data_list,
+#      .data = list(
+#        duplicates = duplicates,
+#        sample_id = "sample_id",
+#        citekey = "citekey",
+#        es_data = str_es_data,
+#        data_x = str_data_x,
+#        data_y = str_data_y,
+#        collapse_method = collapse_method,
+#        retain_original = FALSE,
+#        intercor = intercor,
+#        partial_intercor = FALSE,
+#        construct_x = "construct_x",
+#        construct_y = "construct_y",
+#        measure_x = "measure_x",
+#        measure_y = "measure_y",
+#        moderator_names = moderator_names_temp,
+#        es_metric = "r",
+#        ma_method = ma_method,
+#        .dx_internal_designation = d,
+#        str_moderators = str_moderators
+#      )
+#   )
+# 
+# list_of_variables <- c("duplicates", "str_es_data","str_data_x","str_data_y",
+#                        "collapse_method", "intercor","moderator_names_temp","ma_method",
+#                        "d","str_moderators")
+# 
+# 
+# in debugmode `save(list = ls(), file = "collapse_data_list.Rda")`
+
+
+
+
+
