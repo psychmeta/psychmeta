580353360a7c25461bc0d7ff8168da39c70b5b3a
Brenton M. Wiernik <bwiernik@users.noreply.github.com>
2022-04-11 11:46:06 -0400

    Skip simulate_d example if nor1mix not available

--- R/simulate_d.R
+++ R/simulate_d.R
@@ -1151,25 +1151,27 @@ append_dmat <- function(di_mat, da_mat,
 #' @importFrom progress progress_bar
 #'
 #' @examples
-#' ## Define sample sizes, means, and other parameters for each of two groups:
-#' n_params <- list(c(mean = 200, sd = 20),
-#'                  c(mean = 100, sd = 20))
-#' rho_params <- list(list(c(.3, .4, .5)),
-#'                    list(c(.3, .4, .5)))
-#' mu_params <- list(list(c(mean = .5, sd = .5), c(-.5, 0, .5)),
-#'                   list(c(mean = 0, sd = .5), c(-.2, 0, .2)))
-#' sigma_params <- list(list(1, 1),
-#'                      list(1, 1))
-#' rel_params <- list(list(.8, .8),
-#'                    list(.8, .8))
-#' sr_params <- list(1, .5)
+#' if (requireNamespace("nor1mix", quietly = TRUE)) {
+#'   ## Define sample sizes, means, and other parameters for each of two groups:
+#'   n_params <- list(c(mean = 200, sd = 20),
+#'                    c(mean = 100, sd = 20))
+#'   rho_params <- list(list(c(.3, .4, .5)),
+#'                      list(c(.3, .4, .5)))
+#'   mu_params <- list(list(c(mean = .5, sd = .5), c(-.5, 0, .5)),
+#'                     list(c(mean = 0, sd = .5), c(-.2, 0, .2)))
+#'   sigma_params <- list(list(1, 1),
+#'                        list(1, 1))
+#'   rel_params <- list(list(.8, .8),
+#'                      list(.8, .8))
+#'   sr_params <- list(1, .5)
 #'
-#' simulate_d_database(k = 5, n_params = n_params, rho_params = rho_params,
-#'                     mu_params = mu_params, sigma_params = sigma_params,
-#'                     rel_params = rel_params, sr_params = sr_params,
-#'                     k_items = c(4, 4),
-#'                     group_names = NULL, var_names = c("y1", "y2"),
-#'                     show_applicant = TRUE, keep_vars = c("y1", "y2"), decimals = 2)
+#'   simulate_d_database(k = 5, n_params = n_params, rho_params = rho_params,
+#'                       mu_params = mu_params, sigma_params = sigma_params,
+#'                       rel_params = rel_params, sr_params = sr_params,
+#'                       k_items = c(4, 4),
+#'                       group_names = NULL, var_names = c("y1", "y2"),
+#'                       show_applicant = TRUE, keep_vars = c("y1", "y2"), decimals = 2)
+#' }
 simulate_d_database <- function(k, n_params, rho_params,
                                 mu_params = NULL, sigma_params = 1,
                                 rel_params = 1, sr_params = 1, k_items_params = 1,
